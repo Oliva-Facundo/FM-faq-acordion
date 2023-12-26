0282ef9f4fe69a9948deeec5394644f5d738e69b
Kairobisky <facundooliva500@hotmail.com>
2023-12-26 10:23:56 -0300

    prettier

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-const acc = document.getElementsByClassName('question');
+const acc = document.getElementsByClassName("question");
 
 for (let i = 0; i < acc.length; i++) {
-    acc[i].addEventListener('click', function() {
-        
-        for (let j = 0; j < acc.length; j++) {
-            if (j !== i) {
-                acc[j].classList.remove('active');
-                const text = acc[j].nextElementSibling;
-                text.style.display = 'none';
-            }
-        }
+  acc[i].addEventListener("click", function () {
+    for (let j = 0; j < acc.length; j++) {
+      if (j !== i) {
+        acc[j].classList.remove("active");
+        const text = acc[j].nextElementSibling;
+        text.style.display = "none";
+      }
+    }
 
-        this.classList.toggle('active');
-        const text = this.nextElementSibling;
-        if (text.style.display === 'block') {
-            text.style.display = 'none';
-        } else {
-            text.style.display = 'block';
-        }
-    } )
+    this.classList.toggle("active");
+    const text = this.nextElementSibling;
+    if (text.style.display === "block") {
+      text.style.display = "none";
+    } else {
+      text.style.display = "block";
+    }
+  });
 }
